ef49397ac454edb7c4f895ae35d8e837c3da64bd
Fritzss <Sergey.Nik.Krylov@gmail.com>
2020-11-26 14:22:56 +0300

    Update backToHetznerStorageBox.ps1

--- backToHetznerStorageBox.ps1
+++ backToHetznerStorageBox.ps1
@@ -1,5 +1,4 @@
-net use Z: /delete
-net use Z: 
+if (Test-Path Z:\) { net use n: \\<user>.your-storagebox.de\backup<or sub account > /user:user pass /PERSISTENT:YES}
 #from which folder in folder c:\backup to take files
 $pathALL="lt","lt-test"
 $pathALL | % {if (!(Test-Path z:\$_)) {New-Item -Path z:\$_ -ItemType Directory}}
