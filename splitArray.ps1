685800597b97095da6795515aaddd41a62c51d72
Fritzss <Sergey.Nik.Krylov@gmail.com>
2021-01-21 10:08:31 +0300

    Update splitArray.ps1

--- splitArray.ps1
+++ splitArray.ps1
@@ -2,8 +2,6 @@
 
 $arr = @(1,2,3,4,5,6,7,8,9,10)
 $z = 3
-$arr.count % $z
-[math]::Truncate($arr.count/$z)
 
 for ($j=1 ; $j -le [math]::Truncate($arr.count/$z); $j++) {   "NEW: $($j *$z)"
 for ($i= ($j*$z - $z)+1 ; $i -le ($z*$j); $i++) { $arr[$i]}
