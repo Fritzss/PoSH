e400554589dd4cf3d8d9f517614ec45ab12eec6e
Fritzss <Sergey.Nik.Krylov@gmail.com>
2021-01-26 08:05:36 +0300

    Update splitArray.ps1
    add function arrSplit

--- splitArray.ps1
+++ splitArray.ps1
@@ -1,12 +1,27 @@
 # split the array into equal parts
 
-$arr = @(1,2,3,4,5,6,7,8,9,10)
-$z = 3
+$arr = @()
 
-for ($j=1 ; $j -le [math]::Truncate($arr.count/$z); $j++) {   "NEW: $($j *$z)"
-for ($i= ($j*$z - $z)+1 ; $i -le ($z*$j); $i++) { if ($arr[$i-1] ne 0) {$arr[$i -1]}}
+$arr = @(1,2,3,4,5,6,7,8,9,11,13,45,78,89,99) 
+$divider = 4
+
+#Get-Variable arrsplit* | Remove-Variable
+
+function arrSplit($array, $div) {
+$arrays = @()
+$arr = $array
+Get-Variable arrsplit* | Remove-Variable
+for ($i = 1 ; $i -le [math]::Truncate($arr.count/$divider); $i++) { 
+ New-Variable -Name "arrsplit$i" -Value @(for ($j= (($i*$divider - $divider) + 1) ; $j -le ($divider*$i); $j++) { if ($arr[$j-1] -ne $null) {$arr[$j-1]} ;  })} ;
+
+if ($arr.Count % $divider -ne 0) {
+ New-Variable -Name "arrsplit$([math]::Truncate($arr.count/$divider)+1)" -value @(for ($k = ($arr.Count - ($arr.count % $divider -1)); $k -le $arr.Count; $k++) {if ($arr[$k-1] -ne $null) {$arr[$k-1]}})
 }
-# remainder of the division
-if ($arr.Count % $z -ne 0) {
-for ($k = ($arr.Count - $arr.count % $z ); $k -le $arr.Count; $k++) {$arr[$k]}
+
+$arrays = Get-Variable arrsplit* #| Remove-Variable
+return $arrays
 }
+
+# Example
+$arrOfarr = arrSplit -array $arr -div $divider
+$arrOfarr[0].Value | % {$_}
